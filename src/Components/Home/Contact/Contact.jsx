9677be21052da405dcfbcd341a484358ac4cf7f0
Pawel Bozejewski <bozejewski@gmail.com>
2020-08-20 12:57:43 +0200

    form

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -1,19 +1,108 @@
-// import React, { useState } from 'react';
-// import './Contact.scss';
-// import SeparateBar from '../../common/separateBar/separateBar5';
-// import { Formik, Field, Form } from 'formik';
-// import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import './Contact.scss';
+import SeparateBar from '../../common/separateBar/separateBar5';
+import { Formik, useField, Form } from 'formik';
+import { TextField } from '@material-ui/core';
+import * as Yup from 'yup';
 
-// const Contact: React.Fc = () => {
-//   const [data, setData] = useState({
-//     personalDate: '',
-//     phoneNumber: '',
-//     city: '',
-//     message: '',
-//     checkbox: '',
-//   });
-//   const [errors, setErrors] = useState('');
+const CustomTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : '';
+  return (
+    <>
+      <TextField
+        {...field}
+        label={label}
+        helperText={errorText}
+        error={!!errorText}
+        variant="outlined"
+        margin="normal"
+      />
+    </>
+  );
+};
 
+const CustomCheckbox = ({ children, ...props }) => {
+  const [field, meta] = useField(props, 'checkbox');
+  return (
+    <>
+      <label className="checkbox">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div className="contactContainer" id="contact">
+      <SeparateBar title="ZAPRASZAMY DO KONTAKTU." number="5" />
+      <div className="barContainer">
+        <p>PIERWSZY KROK NALEŻY DO CIEBIE. ZAPRASZAMY DO KONTAKTU.</p>
+      </div>
+      <h1>Formularz kontaktowy</h1>
+      <Formik
+        initialValues={{
+          personalDate: '',
+          phoneNumber: '',
+          city: '',
+          message: '',
+          acceptTerms: false,
+        }}
+        validationSchema={Yup.object({
+          personalDate: Yup.string()
+            .typeError('bez CYFERek')
+            .min(6, 'podaj imie')
+            .max(20, 'miał byc poprawny kolego')
+            .required('wymagane i to bardzo '),
+          phoneNumber: Yup.number()
+            .typeError('MAL BYC NUMERRTYLK CYFERKI')
+            .min(6, 'podaj poprawy nr')
+            .max(20, 'miał byc poprawny kolego')
+            .required('wymagane'),
+          acceptTerms: Yup.boolean()
+            .required('musisz sie zgadzac ')
+            .oneOf([true], 'no co jest z a zgoda'),
+        })}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          resetForm();
+          setSubmitting(false);
+        }}
+      >
+        {(props) => (
+          <Form>
+            <CustomTextInput
+              label="Imię i nazwisko"
+              name="personalDate"
+              type="text"
+            />
+            <CustomTextInput
+              label="Numer eletonu"
+              name="phoneNumber"
+              type="text"
+            />
+            <CustomTextInput label="Miejscowość" name="city" type="text" />
+
+            <div className="buttonContainer">
+              <button type="submit">WYŚLIJ</button>
+              <CustomCheckbox name="acceptTerms">
+                Akceptuję politykę prywatności<span>RODO</span>
+              </CustomCheckbox>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Contact;
+
+// const Contact = () => {
 //   return (
 //     <div className="contactContainer" id="contact">
 //       <SeparateBar title="ZAPRASZAMY DO KONTAKTU." number="5" />
@@ -29,13 +118,20 @@
 //           message: '',
 //           acceptTerms: false,
 //         }}
-//         validate={(values) => {
-//           const errors = {};
-//           if ((values.personalDate = 'bob')) {
-//             errors.personalDate = 'pole jest wymagane';
-//           }
-//           return errors;
-//         }}
+//         validationSchema={Yup.object({
+//           personalDate: Yup.string()
+//             .min(5, 'minimum 5 znaków')
+//             .max(30, 'maxymalnie 20 znaków')
+//             .required('kolego pole jest wymagane'),
+//           phoneNumber: Yup.number()
+//             .typeError('MAL BYC NUMERRTYLK CYFERKI')
+//             .min(6, 'podaj poprawy nr')
+//             .max(20, 'miał byc poprawny kolego')
+//             .required('wymagane'),
+//           acceptTerms: Yup.boolean()
+//             .required('musisz sie zgadzac ')
+//             .oneOf([true], 'no co jest z a zgoda'),
+//         })}
 //         onSubmit={(data, { setSubmitting }) => {
 //           setSubmitting(true);
 //           console.log('submit', data);
@@ -94,198 +190,3 @@
 // };
 
 // export default Contact;
-
-import React, { useState } from 'react';
-import './Contact.scss';
-import SeparateBar from '../../common/separateBar/separateBar5';
-import Input from '../../common/input';
-import { Formik, useField, Form } from 'formik';
-import * as Yup from 'yup';
-
-const CustomTextInput = ({ lable, ...props }) => {
-  const [field, meta] = useField(props);
-
-  return (
-    <>
-      <lable htmlFor={props.id || props.name}>{lable}</lable>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
-const CustomCheckbox = ({ children, props }) => {
-  const [field, meta] = useField(props, 'checkbox');
-
-  return (
-    <>
-      <lable className="checkbox">
-        <input type="checkbox" {...field} {...props} />
-        {children}
-      </lable>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
-const Contact = () => {
-  // const [data, setData] = useState({
-  //   personalDate: '',
-  //   phoneNumber: '',
-  //   city: '',
-  //   message: '',
-  //   checkbox: '',
-  // });
-  // const [errors, setErrors] = useState('');
-
-  // let schema = {
-  //   personalDate: Joi.string().required().label('Imię i nazwisko'),
-  //   phoneNumber: Joi.number().required().label('Numer Telefonu'),
-  //   city: Joi.string().required().label(' Miasto'),
-  //   checkbox: Joi.required().label('Pole'),
-  // };
-
-  // let validate = () => {
-  //   // const options = { abortEarly: false };
-  //   // const { error } = Joi.validate(data, schema, options);
-  //   // if (!error) return null;
-
-  //   const errors = {};
-
-  //   if (data.personalDate.trim() === '') {
-  //     errors.personalDate = 'Imie i nazwiskosa wymagane';
-  //   }
-  //   if (data.phoneNumber.trim() === '') {
-  //     errors.phoneNumber = 'pole telefon jest wymagane ';
-  //   }
-
-  //   return Object.keys(errors).length === 0 ? null : errors;
-  // };
-
-  // //   const errors = {};
-  // //   for (let item of error.detalis) errors[item.path[0]] = item.message;
-  // //   return errors;
-  // // };
-
-  // // let validateProprty = ({ name, value }) => {
-  // //   const obj = { [name]: value };
-  // //   const schemaA = { [name]: schema[name] };
-  // //   const { error } = Joi.validate(obj, schemaA);
-  // //   return error ? error.detalis[0].message : null;
-  // // };
-  // const handleChange = (e) => {
-  //   //   const errorss = { ...errors };
-  //   //   const errorMessage = this.validateProperty(event);
-  //   //   if (errorMessage) errorss[event.name] = errorMessage;
-  //   //   else delete errorss[event.name];
-  //   //   const data = { data };
-  //   //   data[event.name] = event.value;
-  //   //   this.setState({ data, errors });
-  //   setData({
-  //     ...data,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   const errors = validate();
-  //   setErrors(errors || {});
-  //   if (errors) return;
-  // };
-
-  return (
-    <div className="contactContainer" id="contact">
-      <SeparateBar title="ZAPRASZAMY DO KONTAKTU." number="5" />
-      <div className="barContainer">
-        <p>PIERWSZY KROK NALEŻY DO CIEBIE. ZAPRASZAMY DO KONTAKTU.</p>
-      </div>
-      <h1>Formularz kontaktowy</h1>
-      {/* <form className="formContainer" onSubmit={handleSubmit}>
-        <Input
-          name="personalDate"
-          label="Imie i Nazwisko"
-          value={data.personalDate}
-          onChange={handleChange}
-          type="text"
-          error={errors.personalDate}
-        />
-        <Input
-          name="phoneNumber"
-          label="Numer telefonu"
-          value={data.phoneNumber}
-          onChange={handleChange}
-          type="text"
-          error={errors.phoneNumber}
-        />
-        <Input
-          name="city"
-          label="Miejscowość"
-          value={data.city}
-          onChange={handleChange}
-          type="text"
-        />
-        <div className="inputContainer">
-          <label>Wiadomość</label>
-          <textarea></textarea>
-        </div>
-        <div className="buttonContainer">
-          <button>WYŚLIJ</button>
-          <input name="checkbox" value={data.checkbox} type="checkbox" />
-          <p>
-            Akceptuję politykę prywatności<span>RODO</span>
-          </p>
-        </div>
-      </form> */}
-      <Formik
-        initialValues={{
-          personalDate: '',
-          phoneNumber: '',
-          city: '',
-          message: '',
-          acceptTerms: false,
-        }}
-        validationSchema={Yup.object({
-          personalDate: Yup.string()
-            .min(5, 'minimum 5 znaków')
-            .max(30, 'maxymalnie 20 znaków')
-            .required('kolego pole jest wymagane'),
-          phoneNumber: Yup.number()
-            .min(6, 'podaj poprawy nr')
-            .max(20, 'miał byc poprawny kolego')
-            .required('wymagane'),
-          acceptTerms: Yup.boolean()
-            .required('musisz sie zgadzac ')
-            .oneOf([true], 'no co jest z a zgoda'),
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          resetForm();
-          setSubmitting(false);
-        }}
-      >
-        {(props) => (
-          <Form>
-            <CustomTextInput
-              lable="Imię i nazwisko"
-              name="personalDetalis"
-              type="text"
-            />
-            <CustomTextInput
-              lable="Numer eletonu"
-              name="phoneNumber"
-              type="text"
-            />
-            <CustomTextInput lable="Miejscowość" name="city" type="text" />
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
-};
-
-export default Contact;
